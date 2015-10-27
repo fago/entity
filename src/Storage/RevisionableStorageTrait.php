97cd47762ddde756bbec2997a9830d0a7b54bc29
Daniel Wehner <daniel@tag1consulting.com>
2015-10-26 20:08:21 -0700

    convert revisionIDs to use EQ

--- src/Storage/RevisionableStorageTrait.php
+++ src/Storage/RevisionableStorageTrait.php
@@ -40,19 +40,11 @@ trait RevisionableStorageTrait {
    * {@inheritdoc}
    */
   public function revisionIds(ContentEntityInterface $entity) {
-    // @todo Too bad that you cannot use an entity query here.
-    $entity_type = $entity->getEntityType();
-    $revision_table = $entity_type->getRevisionTable();
-    $revision_field = $entity_type->getKey('revision');
-    $id_field = $entity_type->getKey('id');
-    $result = $this->database->select($revision_table)
-      ->fields($revision_table, [$revision_field])
-      ->condition($id_field, $entity->id())
-      ->execute()
-      ->fetchAllKeyed(0, 0);
-
-    return array_values($result);
+    $result = $this->getQuery()
+      ->allRevisions()
+      ->condition($entity_type->getKey('id'), $entity->id())
+      ->execute();
+    return array_keys($result);
   }
-
 
 }
