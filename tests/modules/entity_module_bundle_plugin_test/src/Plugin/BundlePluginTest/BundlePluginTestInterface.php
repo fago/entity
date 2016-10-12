459c33bcd3102c7d7c725b4da77acab1de0105b3
Bojan Zivanovic <bojanz@gmail.com>
2016-10-12 11:34:51 +0200

    Issue #2817751 by bojanz: Create an API for bundle plugins

---
+++ tests/modules/entity_module_bundle_plugin_test/src/Plugin/BundlePluginTest/BundlePluginTestInterface.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace Drupal\entity_module_bundle_plugin_test\Plugin\BundlePluginTest;
+
+use Drupal\entity\BundlePlugin\BundlePluginInterface;
+
+interface BundlePluginTestInterface extends BundlePluginInterface {
+
+}
