ec97e5870a888f5b5d763363f3b70c7d0cd115c4
bojanz <bojanz@86106.no-reply.drupal.org>
2018-06-08 11:46:38 +0200

    Issue #2977381 by bojanz: Reduce duplication between EntityAccessControlHandler and UncacheableEntityAccessControlHandler

--- src/UncacheableEntityAccessControlHandler.php
+++ src/UncacheableEntityAccessControlHandler.php
@@ -17,7 +17,7 @@ use Drupal\user\EntityOwnerInterface;
  *
  * Note: this access control handler will cause pages to be cached per user.
  */
-class UncacheableEntityAccessControlHandler extends CoreEntityAccessControlHandler {
+class UncacheableEntityAccessControlHandler extends EntityAccessControlHandlerBase {
 
   /**
    * {@inheritdoc}
@@ -33,59 +33,6 @@ class UncacheableEntityAccessControlHandler extends CoreEntityAccessControlHandl
   /**
    * {@inheritdoc}
    */
-  protected function checkAccess(EntityInterface $entity, $operation, AccountInterface $account) {
-    $account = $this->prepareUser($account);
-    /** @var \Drupal\Core\Access\AccessResult $result */
-    $result = parent::checkAccess($entity, $operation, $account);
-
-    if ($result->isNeutral()) {
-      if ($entity instanceof EntityOwnerInterface) {
-        $result = $this->checkEntityOwnerPermissions($entity, $operation, $account);
-      }
-      else {
-        $result = $this->checkEntityPermissions($entity, $operation, $account);
-      }
-    }
-
-    // Ensure that access is evaluated again when the entity changes.
-    return $result->addCacheableDependency($entity);
-  }
-
-  /**
-   * Checks the entity operation and bundle permissions.
-   *
-   * @param \Drupal\Core\Entity\EntityInterface $entity
-   *   The entity for which to check access.
-   * @param string $operation
-   *   The entity operation. Usually one of 'view', 'view label', 'update' or
-   *   'delete'.
-   * @param \Drupal\Core\Session\AccountInterface $account
-   *   The user for which to check access.
-   *
-   * @return \Drupal\Core\Access\AccessResultInterface
-   *   The access result.
-   */
-  protected function checkEntityPermissions(EntityInterface $entity, $operation, AccountInterface $account) {
-    return AccessResult::allowedIfHasPermissions($account, [
-      "$operation {$entity->getEntityTypeId()}",
-      "$operation {$entity->bundle()} {$entity->getEntityTypeId()}",
-    ], 'OR');
-  }
-
-  /**
-   * Checks the entity operation and bundle permissions, with owners.
-   *
-   * @param \Drupal\Core\Entity\EntityInterface $entity
-   *   The entity for which to check access.
-   * @param string $operation
-   *   The entity operation. Usually one of 'view', 'view label', 'update' or
-   *   'delete'.
-   * @param \Drupal\Core\Session\AccountInterface $account
-   *   The user for which to check access.
-   *
-   * @return \Drupal\Core\Access\AccessResultInterface
-   *   The access result.
-   */
   protected function checkEntityOwnerPermissions(EntityInterface $entity, $operation, AccountInterface $account) {
     /** @var \Drupal\user\EntityOwnerInterface $entity */
     if ($operation === 'view' && $entity instanceof EntityPublishedInterface && !$entity->isPublished()) {
@@ -100,42 +47,7 @@ class UncacheableEntityAccessControlHandler extends CoreEntityAccessControlHandl
       $result = AccessResult::allowedIfHasPermissions($account, $permissions)->cachePerUser();
     }
     else {
-      if ($account->id() == $entity->getOwnerId()) {
-        $permissions = [
-          "$operation own {$entity->getEntityTypeId()}",
-          "$operation any {$entity->getEntityTypeId()}",
-          "$operation own {$entity->bundle()} {$entity->getEntityTypeId()}",
-          "$operation any {$entity->bundle()} {$entity->getEntityTypeId()}",
-        ];
-      }
-      else {
-        $permissions = [
-          "$operation any {$entity->getEntityTypeId()}",
-          "$operation any {$entity->bundle()} {$entity->getEntityTypeId()}",
-        ];
-      }
-
-      $result = AccessResult::allowedIfHasPermissions($account, $permissions, 'OR')->cachePerUser();
-    }
-
-    return $result;
-  }
-
-  /**
-   * {@inheritdoc}
-   */
-  protected function checkCreateAccess(AccountInterface $account, array $context, $entity_bundle = NULL) {
-    $result = parent::checkCreateAccess($account, $context, $entity_bundle);
-    if ($result->isNeutral()) {
-      $permissions = [
-        'administer ' . $this->entityTypeId,
-        'create ' . $this->entityTypeId,
-      ];
-      if ($entity_bundle) {
-        $permissions[] = 'create ' . $entity_bundle . ' ' . $this->entityTypeId;
-      }
-
-      $result = AccessResult::allowedIfHasPermissions($account, $permissions, 'OR');
+      $result = parent::checkEntityOwnerPermissions($entity, $operation, $account);
     }
 
     return $result;
